b70f1d42e5f3548c8154866a78c93a6ec3436a40
12shivamkumar <shivam.kumar@capillarytech.com>
2022-09-15 19:29:08 +0530

    drop column from sql file

--- schema/campaigns/campaign_meta_details/bucket_details.sql
+++ schema/campaigns/campaign_meta_details/bucket_details.sql
@@ -2,8 +2,6 @@
 
 CREATE TABLE `bucket_details` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `shard_id` int(11) NOT NULL,
-  `hit_rate` int(11) NOT NULL,
   `rows_count` int(11) DEFAULT NULL,
   `last_visited_on` date NOT NULL,
   PRIMARY KEY (`id`)
