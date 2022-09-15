cc27e2de93d356c61a02deb32a43cf98b4df892a
12shivamkumar <shivam.kumar@capillarytech.com>
2022-09-15 19:27:05 +0530

    changes validator files

--- validator/change-files.sh
+++ validator/change-files.sh
@@ -3,8 +3,6 @@
 declare -a files_list
 files_list=$1
 
-echo ${files_list}
-
 declare -A schema_list
 for i in ${files_list[*]}
 do
@@ -14,16 +12,11 @@ if [[ "$i" =~ ^schema.* ]]; then
 fi
 done
 
-echo ${schema_list[@]}
 
 change_in_db_files=false
-echo $change_in_db_files
 if [[ ${#schema_list[@]} -gt 0 ]]; then
   change_in_db_files=true
 fi
-
-echo ${schema_list[@]}
-echo $change_in_db_files
 
 echo "::set-output name=schema_files::" "${schema_list[@]}"
 echo "::set-output name=db_file_change_exists::$change_in_db_files"
